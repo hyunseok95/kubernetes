9481607e35ed75addbbaad1b3d9cd638a251ec60
hyunseok95 <hyunseok95@gmail.com>
2022-10-07 01:08:09 +0900

    update installer

--- installer/kubespray/ansible_install.sh
+++ installer/kubespray/ansible_install.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 
-sudo apt-get update && sudo apt-get install -y runc
-
-mkdir -p $HOME/workspace && cd $HOME/workspace
-git clone https://github.com/kubernetes-sigs/kubespray.git
-sudo apt-get update && sudo apt-get install -y python3-virtualenv
-
 VENVDIR=kubespray-venv
 KUBESPRAYDIR=kubespray
 ANSIBLE_VERSION=2.12
 
-virtualenv --python=$(which python3) $VENVDIR && source $VENVDIR/bin/activate
+sudo apt-get update  
+sudo apt-get install -y runc python3-virtualenv
+
+git clone https://github.com/kubernetes-sigs/kubespray.git
+virtualenv --python=$(which python3) $VENVDIR 
+source $VENVDIR/bin/activate
 
 cd $KUBESPRAYDIR
 pip install -U -r requirements-$ANSIBLE_VERSION.txt
 
 cp -rfp inventory/sample inventory/kubernetes-cluster
-vi inventory/kubernetes-cluster/hosts.yaml
+cat hosts.yaml | tee inventory/kubernetes-cluster/hosts.yaml > /dev/null
 ansible-playbook -i inventory/kubernetes-cluster/hosts.yaml --become --become-user=root cluster.yml
 
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+# To make kubectl work for your non-root user
+USERS_NAME=$(who am i | awk '{print $1}')
+sudo mkdir -p /home/"$USERS_NAME"/.kube
+sudo cp -i /etc/kubernetes/admin.conf /home/"$USERS_NAME"/.kube/config
+sudo chown "$USERS_NAME":"$USERS_NAME" /home/"$USERS_NAME"/.kube/config
